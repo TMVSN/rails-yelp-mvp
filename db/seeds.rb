07f32b44ed994e3fcefd8ee32be309b313ce9cd2
Thomas Monvoisin <thomasmonvoisin@gmail.com>
2020-11-11 13:47:27 +0100

    Finished v1 of Yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Restaurant.destroy_all
+Review.destroy_all
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.phone_number
+
+  )
+  restaurant.save!
+  # 5.times do
+  #   review = Review.new(
+  #     rating: rand(0..5),
+  #     content: Faker::Restaurant.review 
+  #   )
+  #   review.restaurant = restaurant
+  #   review.save!
+  # end
+end
